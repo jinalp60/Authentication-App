3df585a3a7bdfc173e943715bffb249846b6c8e5
jinalp60 <jinal_p60@yahoo.com>
2022-10-08 12:39:29 -0400

    todo-app minor changes

--- Frontend/src/app/app.component.spec.ts
+++ Frontend/src/app/app.component.spec.ts
@@ -32,6 +32,6 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('Authentication-App app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('ToDo-App is running!');
   });
 });
